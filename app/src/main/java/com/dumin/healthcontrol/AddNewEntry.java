f13291fa89fcb0c52da37ae65332a8086940c8a1
rb <msg-rb@ya.ru>
2017-12-26 07:21:21 +0300

    Add New entry activity

---
+++ app/src/main/java/com/dumin/healthcontrol/AddNewEntry.java
@@ -0,0 +1,30 @@
+package com.dumin.healthcontrol;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class AddNewEntry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_entry);
+
+        // Back arrow in action bar
+        ActionBar actionBar =getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/com/dumin/healthcontrol/MainActivity.java
+++ app/src/main/java/com/dumin/healthcontrol/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dumin.healthcontrol;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,8 +30,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, AddNewEntry.class);
+                startActivity(intent);
             }
         });
         // for rendering pages in TabLayout
