8ce5da465fffa0f1a5d84412ab1572c9e0800f76
rb <msg-rb@ya.ru>
2017-12-27 08:32:28 +0300

    Add View NumberPicker

--- app/src/main/java/com/dumin/healthcontrol/AddNewEntry.java
+++ app/src/main/java/com/dumin/healthcontrol/AddNewEntry.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
 
 public class AddNewEntry extends AppCompatActivity {
 
@@ -16,6 +17,21 @@ public class AddNewEntry extends AppCompatActivity {
         ActionBar actionBar =getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        NumberPicker numberPicker0 = (NumberPicker) findViewById(R.id.np_systolic_pressure);
+        numberPicker0.setMaxValue(300);
+        numberPicker0.setMinValue(50);
+        numberPicker0.setValue(120);
+
+        NumberPicker numberPicker1 = (NumberPicker) findViewById(R.id.np_diastolic_pressure);
+        numberPicker1.setMaxValue(150);
+        numberPicker1.setMinValue(30);
+        numberPicker1.setValue(80);
+
+        NumberPicker numberPicker2 = (NumberPicker) findViewById(R.id.np_pulse);
+        numberPicker2.setMaxValue(500);
+        numberPicker2.setMinValue(40);
+        numberPicker2.setValue(70);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
