2783c284562a6938fda3361dff1a0301cd4bfdc8
rb <msg-rb@ya.ru>
2017-12-27 10:54:49 +0300

    Add View NumberPicker

--- app/src/main/java/com/dumin/healthcontrol/MainActivity.java
+++ app/src/main/java/com/dumin/healthcontrol/MainActivity.java
@@ -3,7 +3,6 @@ package com.dumin.healthcontrol;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.View;
